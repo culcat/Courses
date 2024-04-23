6540659601821fb25302709e1c0541515fd95beb
Aleksandr Krainukov <krainukovaleksandr1@gmail.com>
2024-04-23 11:46:56 +0300

    сделал вью функции и шаблоны

--- Courses/urls.py
+++ Courses/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for Courses project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from CoursesApp.views import home, course_list, course_detail, my_courses, lesson_detail, submit_answer, rate_answers, student_ranking
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('/admin', admin.site.urls),
+    path('courses/', course_list, name='course_list'),
+    path('course/<int:course_id>/', course_detail, name='course_detail'),
+    path('my_courses/', my_courses, name='my_courses'),
+    path('lesson/<int:lesson_id>/', lesson_detail, name='lesson_detail'),
+    path('submit_answer/<int:lesson_id>/', submit_answer, name='submit_answer'),
+    path('rate_answers/', rate_answers, name='rate_answers'),
+    path('student_ranking/', student_ranking, name='student_ranking'),
 ]
--- CoursesApp/views.py
+++ CoursesApp/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Course, Lesson, StudentAnswer, StudentRating
 
-# Create your views here.
+def home(request):
+    popular_courses = Course.objects.all()[:5]
+    return render(request, 'home.html', {'popular_courses': popular_courses})
+
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, 'course_list.html', {'courses': courses})
+
+def course_detail(request, course_id):
+    course = Course.objects.get(id=course_id)
+    lessons = Lesson.objects.filter(course=course)
+    return render(request, 'course_detail.html', {'course': course, 'lessons': lessons})
+
+@login_required
+def my_courses(request):
+    user = request.user
+    if user.user_type == 'student':
+        courses = Course.objects.filter(student=user)
+        return render(request, 'my_courses.html', {'courses': courses})
+    else:
+        return redirect('home')
+
+@login_required
+def lesson_detail(request, lesson_id):
+    lesson = Lesson.objects.get(id=lesson_id)
+    return render(request, 'lesson_detail.html', {'lesson': lesson})
+
+@login_required
+def submit_answer(request, lesson_id):
+    if request.method == 'POST':
+        user = request.user
+        lesson = Lesson.objects.get(id=lesson_id)
+        answer_file = request.FILES['answer']
+        StudentAnswer.objects.create(student=user, lesson=lesson, answer_file=answer_file)
+        return redirect('lesson_detail', lesson_id=lesson_id)
+    else:
+        return redirect('home')
+
+@login_required
+def rate_answers(request):
+    if request.user.user_type == 'teacher':
+        if request.method == 'POST':
+            lesson_id = request.POST.get('lesson_id')
+            lesson = Lesson.objects.get(id=lesson_id)
+            if lesson.course.teacher != request.user:
+                return redirect('home')
+
+            # Получаем данные из POST-запроса
+            answer_id = request.POST.get('answer_id')
+            score = int(request.POST.get('score'))
+
+            # Находим ответ студента и выставляем баллы
+            student_answer = StudentAnswer.objects.get(id=answer_id)
+            student_answer.score = score
+            student_answer.save()
+
+            # Рассчитываем общий балл ученика и обновляем его рейтинг
+            student = student_answer.student
+            total_score = StudentAnswer.objects.filter(student=student).aggregate(total_score=models.Sum('score'))['total_score']
+            student_rating, created = StudentRating.objects.get_or_create(student=student)
+            student_rating.score = total_score
+            student_rating.save()
+            pass
+        else:
+            user = request.user
+            courses_taught = Course.objects.filter(teacher=user)
+            student_answers = StudentAnswer.objects.filter(lesson__course__in=courses_taught)
+            return render(request, 'rate_answers.html', {'student_answers': student_answers})
+    else:
+        return redirect('home')
+
+def student_ranking(request):
+    top_students = StudentRating.objects.order_by('-score')[:50]
+    return render(request, 'student_ranking.html', {'top_students': top_students})
