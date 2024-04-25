c3efadb92b8eb6250fbbda83e17a51a8c2db4b19
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2024-04-26 00:35:25 +0300

    пофиксил бд

--- CoursesApp/models.py
+++ CoursesApp/models.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import AbstractUser,Group,Permission
 from django.db import models
 from django.utils.translation import gettext as _
+
+class EducationalOrganization(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    def __str__(self):
+        return self.name
+
 class CustomUser(AbstractUser):
     USER_TYPE_CHOICES = [
         ('admin', 'Админ'),
@@ -23,13 +32,6 @@ class CustomUser(AbstractUser):
 
 
 
-class EducationalOrganization(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
-    phone = models.CharField(max_length=20)
-    email = models.EmailField()
-    def __str__(self):
-        return self.name
 
 
 class Course(models.Model):
@@ -38,19 +40,23 @@ class Course(models.Model):
     teacher = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='courses_taught')
     organization = models.ForeignKey(EducationalOrganization, on_delete=models.CASCADE, related_name='courses')
     max_score = models.IntegerField(default=10)
+    def __str__(self):
+        return self.name
 
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
     title = models.CharField(max_length=200)
     material = models.TextField()
-
+    def __str__(self):
+        return self.title
 
 class StudentAnswer(models.Model):
     student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='answers')
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='answers')
     answer_file = models.FileField(upload_to='answers/')
     score = models.IntegerField(null=True, blank=True)
+
 
 
 class StudentRating(models.Model):
